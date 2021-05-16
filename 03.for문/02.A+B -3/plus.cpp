05b3d820270f2745d5dd22a4071fcd372615ed82
Hajin Kim <rhrnak624@naver.com>
2021-05-16 23:27:16 +0900

    0516

---
+++ 03.for문/02.A+B -3/plus.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt;
+    cin >> cnt;
+    for(int i=0; i<cnt; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
