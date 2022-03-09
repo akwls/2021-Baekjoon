aca3a245c3195451764f293a692111739e22e246
Hajin Kim <rhrnak624@naver.com>
2022-03-10 00:08:21 +0900

    스택 01, 02

---
+++ 18. 스택/02. 제로/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int length;
+	cin >> length;
+
+	vector<int> arr;
+
+	for (int i = 0; i < length; i++) {
+		int n;
+		cin >> n;
+		if (n != 0) {
+			arr.push_back(n);
+		}
+		else {
+			arr.pop_back();
+		}
+	}
+
+	int result = 0;
+	for (auto item : arr) {
+		result += item;
+	}
+	cout << result;
+}
