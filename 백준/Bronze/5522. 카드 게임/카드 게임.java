d215efafec92a708713d9a6953f941dd688a1585
Hajin Kim <80630432+akwls@users.noreply.github.com>
2022-03-29 19:24:26 +0900

    [Bronze V] Title: 카드 게임, Time: 112 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+        for(int i=0; i<5; i++) {
+            answer += sc.nextInt();
+        }
+        System.out.println(answer);
+    }
+}
